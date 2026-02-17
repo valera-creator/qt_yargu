35a8d8e752e843598c37b8c6838d91272e209681
Valera <valerylarionov06@gmail.com>
2026-02-17 19:34:09 +0300

    3 лаба

--- laba3/task_2.py
+++ laba3/task_2.py
@@ -1,5 +1,4 @@
-from PySide6.QtWidgets import QApplication, QLabel, QVBoxLayout, QHBoxLayout, QWidget, \
-    QCheckBox, QSpinBox
+from PySide6.QtWidgets import QApplication, QLabel, QVBoxLayout, QHBoxLayout, QWidget, QCheckBox, QSpinBox
 
 
 class Window(QWidget):
@@ -7,21 +6,21 @@ class Window(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Продукты")
-        self.setMinimumSize(300, 300)
-        self.min_size_box = 120
+        self.setMinimumSize(400, 400)
+        self.min_size_box = 55
         self.max_products = 100
-        self.vertical_layout = QVBoxLayout()
+        self.main_vertical_layout = QVBoxLayout()
 
-        self.products = []  # список картежей вида (цена, checkbox с галочкой, spinbox с количеством)
+        self.products = []  # список картежей вида (цена, название, checkbox с галочкой, spinbox с количеством)
         self.create_product("Молоко", 100)
         self.create_product("Хлеб", 50)
         self.create_product("Сыр", 75)
         self.create_product("Шампанское", 550)
 
         self.label_cost = QLabel("Текущая стоимость покупки: 0")
-        self.vertical_layout.addWidget(self.label_cost)
+        self.main_vertical_layout.addWidget(self.label_cost)
 
-        self.setLayout(self.vertical_layout)
+        self.setLayout(self.main_vertical_layout)
 
     def create_product(self, name, cost):
         horizontal_layout = QHBoxLayout()
@@ -37,32 +36,37 @@ class Window(QWidget):
         # размещение
         horizontal_layout.addWidget(checkbox)
         horizontal_layout.addWidget(spinbox)
-        self.vertical_layout.addLayout(horizontal_layout)
+        self.main_vertical_layout.addLayout(horizontal_layout)
 
         # пересчёт цены после изменения хотя бы одной штуки
         checkbox.stateChanged.connect(self.calculate_total)
         spinbox.valueChanged.connect(self.calculate_total)
 
-        self.products.append((cost, checkbox, spinbox))
+        self.products.append((cost, name, checkbox, spinbox))
 
     def clicked_chebox(self):
         cur_product = self.sender()  # текущий элемент checkbox, с которого было нажато
         value = cur_product.checkState().value == 2  # значение 2 если выбран, 0 если не выбран
+
         font = cur_product.font()
         font.setBold(value)  # в зависимости от выбранного значение переменная bool True или False
         cur_product.setFont(font)
 
     def calculate_total(self):
-        cur_cost = 0
+        cur_cost_products = 0
+        text = ""
         for elem in self.products:
-            cost, checkbox, spinbox = elem[0], elem[1], elem[2]
-            cur_cost += cost * int(checkbox.checkState().value == 2) * int(spinbox.value())
-        self.label_cost.setText(f"Текущая стоимость покупки: {cur_cost}")
+            cost, name, checkbox, spinbox = elem[0], elem[1], elem[2], elem[3]
+
+            if int(spinbox.value()) != 0 and checkbox.checkState().value == 2:  # если продукт выбран
+                text += f"{name}: {cost} * {int(spinbox.value())} = {cost * int(spinbox.value())}\n"
+
+            cur_cost_products += cost * int(checkbox.checkState().value == 2) * int(spinbox.value())
+        self.label_cost.setText(f"{text}Текущая стоимость покупки: {cur_cost_products}")
 
 
 if __name__ == '__main__':
     app = QApplication([])
     window = Window()
     window.show()
-
     app.exec()
---
+++ laba3/task_3.py
@@ -0,0 +1,47 @@
+from PySide6.QtCore import QDate, Qt, QDateTime
+from PySide6.QtWidgets import QApplication, QWidget, QLabel, QDateTimeEdit, QVBoxLayout, QTextEdit
+import datetime
+
+
+class Window(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Дата рождения")
+        self.setMinimumSize(350, 350)
+
+        self.vertical_layout = QVBoxLayout()
+        self.vertical_layout.setAlignment(Qt.AlignTop | Qt.AlignHCenter)
+        self.setLayout(self.vertical_layout)
+
+        self.label_text = QLabel("Выберите дату рождения")
+
+        self.dateTimeEdit = QDateTimeEdit(QDate.currentDate(), self)
+        self.dateTimeEdit.setMinimumDate(QDate.currentDate().addDays(-365 * 120))
+        self.dateTimeEdit.setMaximumDateTime(QDateTime.currentDateTime())
+        self.dateTimeEdit.setDisplayFormat("yyyy.MM.dd hh:mm:ss")
+        self.dateTimeEdit.setCalendarPopup(True)
+        self.dateTimeEdit.dateTimeChanged.connect(self.calculate_diff)
+
+        self.textEdit = QTextEdit()
+        self.textEdit.setReadOnly(True)
+
+        self.vertical_layout.addWidget(self.label_text)
+        self.vertical_layout.addWidget(self.dateTimeEdit)
+        self.vertical_layout.addWidget(self.textEdit)
+
+    def calculate_diff(self):
+        cur_datetime = datetime.datetime.today()
+        user_datetime = self.dateTimeEdit.dateTime().toPython()  # конверт в формат datetime
+
+        diff = cur_datetime - user_datetime
+        text = (f"Сейчас лет: {diff.days // 365}\nСейчас часов: {int(diff.total_seconds() // 3600)}\n"
+                f"Сейчас секунд: {int(diff.total_seconds())}")
+        self.textEdit.setText(text)
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+    window = Window()
+    window.show()
+    app.exec()
