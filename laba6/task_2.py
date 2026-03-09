de4f4c0635fed9b012d9140a033224b20e8cccd6
Valera <valerylarionov06@gmail.com>
2026-03-09 18:27:50 +0300

    6 лаба: 2 номер исправление 2.0

--- laba6/task_2.py
+++ laba6/task_2.py
@@ -27,7 +27,10 @@ class Window(QMainWindow):
         self.widget = QWidget()
         self.widget.setLayout(self.v_layout)
         self.setCentralWidget(self.widget)
-        self.setLayout(self.v_layout)
+
+        # изменение редактирования в верхней панели
+        # срабатывает в любом случае, когда меняется текущий индекс currentIndex
+        self.view.selectionModel().currentChanged.connect(self.update_edit_menu_state)
 
     def add_up_menu(self):
         menubar = self.menuBar()
@@ -35,9 +38,21 @@ class Window(QMainWindow):
         action_create_menu = menu_create.addAction("Создать")
         action_create_menu.triggered.connect(self.create_note)
 
+        # редактировать в верхнем меню
+        self.action_edit = menu_create.addAction("Редактировать")
+        self.action_edit.triggered.connect(lambda: self.edit_note(self.view.currentIndex()))
+        self.action_edit.setEnabled(False)  # по умолчанию неактивен
+        self.action_edit.setVisible(False)  # по умолчанию невидимый
+
+    def update_edit_menu_state(self):
+        # Обновляет доступность пункта "Редактировать" в верхнем меню, если есть выделенный элемент
+        has_selection = self.view.currentIndex().isValid()
+        self.action_edit.setEnabled(has_selection)
+        self.action_edit.setVisible(has_selection)
+
     def contextMenuEvent(self, e):
         context = QMenu(self)
-        click_pos = self.view.viewport().mapFromGlobal(e.globalPos())  # пересчет на координаты окна в виджете
+        click_pos = self.view.viewport().mapFromGlobal(e.globalPos())  # пересчет на координаты окна в виджете, где клик
         index = self.view.indexAt(click_pos)  # элемент в view по координатам
 
         if index.isValid():
@@ -61,12 +76,12 @@ class Window(QMainWindow):
             self.model.append_data(text)
 
     @Slot()
-    def edit_note(self, cur_row):
-        text = self.model.get_text_by_index(cur_row.row())
+    def edit_note(self, index):
+        text = self.model.get_text_by_index(index.row())
         dlg = CustomDialog("Редактирование", text)
         if dlg.exec():
             text = dlg.get_text()
-            self.model.setData(self.view.currentIndex(), text)
+            self.model.setData(index, text)
 
 
 if __name__ == '__main__':
